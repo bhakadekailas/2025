defa4defc49f53815df57387060e77bcdf768022
Kailas Bhakade <k.bhakade349@gmail.com>
2022-04-18 11:04:03 +0530

    Implicit Intent and Explicit Intent module added

---
+++ app/explicitintent/src/main/java/com/example/explicitintent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.buttonNext);
+
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
+    }
+}
