6dd11c28bde06ba6a8d5fcc9a0f767f71a5eb593
Kailas Bhakade <k.bhakade349@gmail.com>
2022-04-26 11:50:57 +0530

    add function created

--- app/calcidemo/src/main/java/com/kailas/calcidemo/MainActivity.java
+++ app/calcidemo/src/main/java/com/kailas/calcidemo/MainActivity.java
@@ -8,22 +8,47 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button_plus, button_minus, button_mul, button_div, button_clear;
     EditText edittext_first_number, edittext_second_number;
     TextView edittext_result;
-    private final String TAG = MainActivity.class.getSimpleName();
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: ");
-
         setContentView(R.layout.activity_main);
         initViews();
+        addClickListeners();
+    }
 
+    private void addClickListeners() {
+        button_plus.setOnClickListener(view -> {
+//            Log.e(TAG, "addClickListeners: ");
+//            Toast.makeText(MainActivity.this, "Add button Clicked", Toast.LENGTH_SHORT).show();
+        });
 
+        button_minus.setOnClickListener(view -> {
+            Log.e(TAG, "addClickListeners: ");
+            Toast.makeText(MainActivity.this, "Sub button Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        button_mul.setOnClickListener(view -> {
+            Log.e(TAG, "addClickListeners: ");
+            Toast.makeText(MainActivity.this, "Mul button Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        button_div.setOnClickListener(view -> {
+            Log.e(TAG, "addClickListeners: ");
+            Toast.makeText(MainActivity.this, "Div button Clicked", Toast.LENGTH_SHORT).show();
+        });
+        button_clear.setOnClickListener(view -> {
+            Log.e(TAG, "addClickListeners: ");
+            Toast.makeText(MainActivity.this, "Clear button Clicked", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void initViews() {
@@ -36,7 +61,11 @@ public class MainActivity extends AppCompatActivity {
         button_clear = findViewById(R.id.button_clear);
 
         // find out edittext
+        edittext_first_number = findViewById(R.id.edittext_first_number);
+        edittext_second_number = findViewById(R.id.edittext_second_number);
 
+        //find out textview
+        edittext_result = findViewById(R.id.edittext_result);
 
     }
 
